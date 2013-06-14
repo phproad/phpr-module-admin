dcffc8d413764c166456a34ac907ba4877006e0e
Sam Georges <sam@daftspunk.com>
2013-06-14 20:27:52 +1000

    Add admin user group model

---
+++ models/admin_group.php
@@ -0,0 +1,13 @@
+<?php
+
+class Admin_Group extends Db_ActiveRecord
+{
+    const admin = 'administrator';
+    
+    public $table_name = 'admin_groups';
+    
+    public static function create($values = null)
+    {
+        return new self($values);
+    }
+}
