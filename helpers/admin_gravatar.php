3e4f44271742c5c8e53c2813b629a7961cd6af58
Matt <matt@mjman.net>
2014-11-20 06:13:12 +1100

    Gravatar Image used for Profile Avatar

---
+++ helpers/admin_gravatar.php
@@ -0,0 +1,15 @@
+<?
+
+class Admin_Gravatar{
+
+        public static function get_img_url($user=null){
+            if(!$user){
+                $user = Phpr::$security->get_user();
+            }
+
+            if(!is_a($user,'Admin_User')){
+                return Admin_Config::get_theme_id().'/assets/images/avatar-blank.png';
+            }
+            return 'http://www.gravatar.com/avatar/'.md5( strtolower( trim( $user->email ) ) );
+        }
+}
